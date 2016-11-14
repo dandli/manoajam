4b18ea27070e72ea516b4a969dedeca0ec5878d9
Azadaj <azad313@hawaii.edu>
2016-11-14 12:51:50 -1000

    Added Authentication

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -5,9 +5,7 @@ import './edit-stuff-page.html';
 import './edit-stuff-page.js';
 
 import './home-page.html';
+import './home-page.js';
 
 import './list-stuff-page.html';
 import './list-stuff-page.js';
-
-import './user-page.html';
-import './user-page.js';
